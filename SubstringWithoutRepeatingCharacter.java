897dd4e8ea17a04f241beb1b7166d524c7402ef1
Rachita Goyal <rachitaagrawal2000@gmail.com>
2022-02-25 19:46:35 +0530

    SubstringWithoutRepeatingCharacter.java

---
+++ SubstringWithoutRepeatingCharacter.java
@@ -0,0 +1,30 @@
+package solved;
+import java.util.*;
+public class SubstringWithoutRepeatingCharacter {
+
+	  public static int lengthOfLongestSubstring(String s) {
+	        HashMap<Character, Integer> map=new HashMap<>();
+	        int count=0;
+	        int ans=0;
+	        for(int i=0; i<s.length();i++)
+	        {
+	        	
+	            if(map.containsKey(s.charAt(i)))
+	            {
+	            	
+	            	i=(map.get(s.charAt(i)));
+	            	
+	                count=0;
+	                map.clear();
+	            }
+	            else{
+	            map.put(s.charAt(i), i);
+	                count++;
+	            }
+	            if(count>ans)
+	                ans=count;
+	        }
+	        return ans;
+	    }
+	 
+}
