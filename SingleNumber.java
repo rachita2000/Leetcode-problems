920cf3a046302f76e9a9cd25ae7b81f1cba85ef0
Rachita Goyal <rachitaagrawal2000@gmail.com>
2022-02-25 19:46:00 +0530

    SingleNumber.java

---
+++ SingleNumber.java
@@ -0,0 +1,13 @@
+package solved;
+
+public class SingleNumber {
+
+	 public int singleNumber(int[] nums) {
+	        int a=nums[0];
+	        for(int i=1;i<nums.length;i++)
+	        {
+	            a=a^nums[i];
+	        }
+	        return a;
+	    }
+}
