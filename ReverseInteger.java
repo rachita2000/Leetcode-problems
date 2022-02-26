9dc95f167c72ecfe360d9be2c16ad6bbedbcbd67
Rachita Goyal <rachitaagrawal2000@gmail.com>
2022-02-26 11:55:48 +0530

    ReverseInteger.java

---
+++ ReverseInteger.java
@@ -0,0 +1,17 @@
+package solved;
+
+
+public int reverse(int x) {
+    long ans=0;
+    while(x!=0)
+    {
+        int r=x%10;
+        ans=ans*10 +r;
+        x/=10;
+    }
+    if(ans > Integer.MAX_VALUE || ans < Integer.MIN_VALUE )
+            return 0;
+    return (int)ans;
+}public class ReverseInteger {
+
+}
