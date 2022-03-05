14cd98f65ca9e4ceb799b1cfa061a9ef1621ba95
Rachita Goyal <rachitaagrawal2000@gmail.com>
2022-03-05 23:06:45 +0530

    ProductOfArrayExceptSelf.java

---
+++ ProductOfArrayExceptSelf.java
@@ -0,0 +1,21 @@
+package solved;
+
+public class ProductOfArrayExceptSelf {
+
+	public int[] productExceptSelf(int[] nums) {
+	    int ans[]=new int[nums.length];
+	        int prefix=1;
+	        for(int i=0;i<nums.length;i++)
+	        {
+	            ans[i]=prefix;
+	            prefix*=nums[i];
+	        }
+	        int postfix=1;
+	        for(int i=nums.length-1;i>=0;i--)
+	        {
+	            ans[i]*=postfix;
+	            postfix*=nums[i];
+	        }
+	        return ans;
+	    }
+}
