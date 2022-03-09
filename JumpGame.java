5459fc76365911823b63dfb1b105ae0c9adc4bdb
Rachita Goyal <rachitaagrawal2000@gmail.com>
2022-03-09 23:16:55 +0530

    JumpGame.java

---
+++ JumpGame.java
@@ -0,0 +1,15 @@
+package solved;
+
+public class JumpGame {
+
+	public boolean canJump(int[] nums) {
+        int possible=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(i>possible)
+                return false;
+            possible=Math.max(possible, i+nums[i]);
+        }
+        return true;
+    }
+}
