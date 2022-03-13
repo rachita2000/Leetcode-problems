c1a0f76bd305b8cd3c2745d102b71321c1aef647
Rachita Goyal <rachitaagrawal2000@gmail.com>
2022-03-13 23:11:22 +0530

    SumRootToLeafNumbers.java

---
+++ SumRootToLeafNumbers.java
@@ -0,0 +1,17 @@
+package solved;
+
+public class SumRootToLeafNumbers {
+
+	 public int sumNumbers(TreeNode root) {
+	        return sumNumbers(root, 0);
+	    }
+	    private int sumNumbers(TreeNode root, int sum) {
+	        if(root==null)
+	            return 0;
+	        sum=(sum*10)+root.val;
+	        if(root.left==null && root.right==null)
+	            return sum;
+	        
+	        return sumNumbers(root.left, sum) + sumNumbers(root.right, sum);
+	    }
+}
