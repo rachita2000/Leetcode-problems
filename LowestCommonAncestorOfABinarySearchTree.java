fb6dddc2bc8e36f102ca33e5dfb95ca937e3c77b
Rachita Goyal <rachitaagrawal2000@gmail.com>
2022-03-20 21:20:21 +0530

    LowestCommonAncestorOfABinarySearchTree.java

---
+++ LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,14 @@
+package solved;
+
+public class LowestCommonAncestorOfABinarySearchTree {
+
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null) return null;
+        if(p.val <root.val && q.val <root.val)
+            return lowestCommonAncestor(root.left, p, q);
+        if(p.val>root.val && q.val>root.val)
+            return lowestCommonAncestor(root.right, p, q);
+        
+        return root;
+    }
+}
