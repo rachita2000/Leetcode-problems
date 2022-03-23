fef45b7a3249212342cdda73b784e1fe037454b5
Rachita Goyal <rachitaagrawal2000@gmail.com>
2022-03-23 18:05:12 +0530

    PalindromeNumber.java

---
+++ PalindromeNumber.java
@@ -0,0 +1,19 @@
+package solved;
+
+public class PalindromeNumber {
+
+	 public boolean isPalindrome(int x) {
+	        if(x<0)
+	            return false;
+	        
+	        int n=x;
+	        int rev=0;
+	        while(n>0)
+	        {
+	            rev=(rev*10)+(n%10);
+	            n/=10;
+	        }
+	        
+	        return rev==x;
+	    }
+}
