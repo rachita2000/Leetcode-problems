bb5d0907ec6b6274ff292f5bd485c761fc5d0101
Rachita Goyal <rachitaagrawal2000@gmail.com>
2022-03-24 22:20:10 +0530

    FindBottomLeftTreeValue.java

---
+++ FindBottomLeftTreeValue.java
@@ -0,0 +1,19 @@
+package solved;
+import java.util.*;
+public class FindBottomLeftTreeValue {
+
+	public int findBottomLeftValue(TreeNode root) {
+	       Queue<TreeNode> q=new LinkedList<>();
+	        q.add(root);
+	        while(!q.isEmpty())
+	        {
+	            root=q.poll();
+	            if(root.right!=null)
+	                q.add(root.right);
+	            if(root.left!=null)
+	                q.add(root.left);
+	        }
+	        
+	        return root.val;
+	    }
+}
