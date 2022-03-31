b206b6dbff0f5071be4a3937b31de3346c8d0232
Rachita Goyal <rachitaagrawal2000@gmail.com>
2022-03-31 22:46:32 +0530

    CountCompleteTreeNodes.java

---
+++ CountCompleteTreeNodes.java
@@ -0,0 +1,10 @@
+package solved;
+
+public class CountCompleteTreeNodes {
+
+	public int countNodes(TreeNode root) {
+        if(root==null) return 0;
+        
+        return countNodes(root.left)+ countNodes(root.right) +1;
+    }
+}
