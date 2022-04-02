e408af47c9b702fdcff1e429f4b7320ae63b14b6
Rachita Goyal <rachitaagrawal2000@gmail.com>
2022-04-02 17:47:11 +0530

    FindAllDuplicatesInAnArray.java

---
+++ FindAllDuplicatesInAnArray.java
@@ -0,0 +1,18 @@
+package solved;
+import java.util.*;
+public class FindAllDuplicatesInAnArray {
+
+	 public List<Integer> findDuplicates(int[] nums) {
+	        List<Integer> ans=new ArrayList<>();
+	        for(int i=0;i<nums.length;i++)
+	        {
+	            int ind=Math.abs(nums[i]) -1;
+	            if(nums[ind] < 0)
+	                ans.add(ind+1);
+	            
+	            nums[ind] *= -1;
+	        }
+	        
+	        return ans;
+	    }
+}
