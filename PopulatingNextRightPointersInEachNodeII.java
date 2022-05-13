9338bd29a676c5142076143705ae222aa9d2dc05
Rachita Goyal <rachitaagrawal2000@gmail.com>
2022-05-13 17:22:10 +0530

    PopulatingNextRightPointersInEachNodeII.java

---
+++ PopulatingNextRightPointersInEachNodeII.java
@@ -0,0 +1,54 @@
+package solved;
+
+public class PopulatingNextRightPointersInEachNodeII {
+
+	public Node connect(Node root) {
+        Node head=root;
+        Node dummy;
+        Node tmp;
+        while(head!=null)
+        {
+            dummy=new Node(0);
+            tmp=dummy;
+            while(head!=null)
+            {
+                if(head.left!=null)
+                {
+                    tmp.next=head.left;
+                    tmp=tmp.next;
+                }
+                if(head.right!=null)
+                {
+                    tmp.next=head.right;
+                    tmp=tmp.next;
+                }
+                
+                head=head.next;
+            }
+            
+            head=dummy.next;
+        }
+        
+        return root;
+    }
+}
+
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+}
