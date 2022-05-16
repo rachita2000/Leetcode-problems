743404632af264c9b0bcef4fcb2e0b872775f2bf
Rachita Goyal <rachitaagrawal2000@gmail.com>
2022-05-16 18:59:40 +0530

    IsSubsequence.java

---
+++ IsSubsequence.java
@@ -0,0 +1,22 @@
+package solved;
+
+public class IsSubsequence {
+
+	public boolean isSubsequence(String s, String t) {
+	      int i=0, j=0;
+	        
+	        while(i<s.length() && j<t.length())
+	        {
+	            if(s.charAt(i)==t.charAt(j))
+	                i++;
+	            
+	            j++;
+	        }
+	        
+	        if(i==s.length())
+	            return true;
+	        
+	        return false;
+	        
+	    }
+}
