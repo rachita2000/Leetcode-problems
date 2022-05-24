9359330b37cc8e7910bd9682a0187781c71913b4
Rachita Goyal <rachitaagrawal2000@gmail.com>
2022-05-24 19:00:27 +0530

    ContainsDuplicateII.java

---
+++ ContainsDuplicateII.java
@@ -0,0 +1,24 @@
+package solved;
+import java.util.*;
+public class ContainsDuplicateII {
+
+	public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map=new HashMap<>();
+        
+        for(int i=0;i<nums.length;i++)
+        {
+            if(map.containsKey(nums[i]))
+            {
+                int a=map.get(nums[i]);
+                int b=i;
+                
+                if(Math.abs(a-b) <=k)
+                    return true;
+            }
+            
+            map.put(nums[i],i);
+        }
+        
+        return false;
+    }
+}
