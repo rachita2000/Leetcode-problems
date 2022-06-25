bef3bad440eae015ec2dfd15786acf6a3aeeb052
Rachita Goyal <rachitaagrawal2000@gmail.com>
2022-06-25 17:34:57 +0530

    NonDecreasingArray.java

--- NonDecreasingArray.java
+++ NonDecreasingArray.java
@@ -2,6 +2,25 @@ package solved;
 
 public class NonDecreasingArray {
 
+	//Optimised
+	public boolean checkPossibilityO(int[] nums) {
+        boolean ans=true;
+        int modification=0;
+        int n=nums.length;
+        for(int i=1;i<n;i++){
+            if(nums[i-1]>nums[i]){
+               modification++;
+            }
+            if(i-2>=0 && i+1<n){
+                if(nums[i-2]>nums[i] && nums[i-1]>nums[i+1])
+                    return false;
+            }
+            if(modification>1)
+                return false;
+        }
+        return true;
+    }
+	//Better
 	public boolean checkPossibility(int[] nums) {
         if(nums.length==1) return true;
         
