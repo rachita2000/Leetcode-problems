027242311bc7be1437ec16fe1ae2ac0003559ea5
Rachita Goyal <rachitaagrawal2000@gmail.com>
2022-06-27 18:09:36 +0530

    PartitioningIntoMinimumNumberOfDeciBinaryNumbers.java

---
+++ PartitioningIntoMinimumNumberOfDeciBinaryNumbers.java
@@ -0,0 +1,14 @@
+package solved;
+
+public class PartitioningIntoMinimumNumberOfDeciBinaryNumbers {
+
+	 public int minPartitions(String n) {
+	        int ans=0;
+	        for(int i=0;i<n.length();i++)
+	        {
+	            int c=n.charAt(i)-'0';
+	            ans= Math.max(ans, c);
+	        }
+	        return ans;
+	    }
+}
