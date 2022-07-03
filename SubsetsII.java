48db13b2521bca377400b1c2f29bb7a003ee8a0e
Rachita Goyal <rachitaagrawal2000@gmail.com>
2022-07-03 22:25:34 +0530

    SubsetsII.java

---
+++ SubsetsII.java
@@ -0,0 +1,26 @@
+package solved;
+import java.util.*;
+public class SubsetsII {
+
+	public List<List<Integer>> subsetsWithDup(int[] nums) {
+        
+        List<List<Integer>> res=new ArrayList<>();
+	        
+	        Arrays.sort(nums);
+	        helper(nums, 0, new ArrayList<>(), res);
+	        
+	        return res;
+   }
+   private void helper(int[] nums, int i, List<Integer> tmp, List<List<Integer>> res) {
+		if(!res.contains(tmp))
+		res.add(new ArrayList<>(tmp));
+		
+		for(int k=i;k<nums.length;k++)
+		{
+			tmp.add(nums[k]);
+			helper(nums, k+1, tmp, res);
+			tmp.remove(tmp.size()-1);
+		}
+		
+	}
+}
